fdcb9e00546e63b3d325848bd2c724f097754460
clydedz <clyd94@gmail.com>
2021-05-24 00:03:50 +1200

    Add deploy section to Kotlin file

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,11 +1,13 @@
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
+import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.finishBuildTrigger
 
 version = "2020.2"
 
 project {
     buildType(Build)
+    buildType(Deploy)
 }
 
 object Build : BuildType({
@@ -38,6 +40,65 @@ object Build : BuildType({
             perCheckinTriggering = true
             groupCheckinsByCommitter = true
             enableQueueOptimization = false
+        }
+    }
+})
+
+object Deploy : BuildType({
+    name = "Deploy"
+
+    enablePersonalBuilds = false
+    type = BuildTypeSettings.Type.DEPLOYMENT
+    maxRunningBuilds = 1
+
+    params {
+        password("service-principal-id", "credentialsJSON:4baaaddb-aef6-478f-97c2-19b06d9677c4")
+        password("service-principal-password", "credentialsJSON:e218e9a3-4fa6-4294-acb6-7c949043f7a0")
+        password("azure-tenant", "credentialsJSON:77e469c2-b099-464f-ad03-8f7c72d5723d")
+    }
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
+
+    steps {
+        script {
+            name = "Check installed Azure CLI version"
+            scriptContent = "az --version"
+        }
+        script {
+            name = "Azure login"
+            scriptContent = "az login --service-principal -u %service-principal-id% -p %service-principal-password% --tenant %azure-tenant%"
+        }
+        script {
+            name = "Update blob storage"
+            scriptContent = "az storage blob upload-batch --account-name craazstoragedemo48765 -d ${'$'}web -s ./target --auth-mode login"
+        }
+        script {
+            name = "Azure logout"
+            scriptContent = "az logout"
+        }
+    }
+
+    triggers {
+        finishBuildTrigger {
+            buildType = "${Build.id}"
+            successfulOnly = true
+            branchFilter = "+:main"
+        }
+    }
+
+    dependencies {
+        dependency(Build) {
+            snapshot {
+                onDependencyFailure = FailureAction.CANCEL
+                onDependencyCancel = FailureAction.CANCEL
+            }
+
+            artifacts {
+                cleanDestination = true
+                artifactRules = "**/*.*=>target"
+            }
         }
     }
 })
