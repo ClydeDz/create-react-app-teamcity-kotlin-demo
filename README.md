94314aead0c6d35b182d2eeb97979ed2a5492e4e
clydedz <clyd94@gmail.com>
2021-05-23 17:54:43 +1200

    Add create react app

---
+++ README.md
@@ -0,0 +1,4 @@
+# Create React App TeamCity Kotlin Demo
+Repository to demonstrate deploying a React app to Azure blob storage via TeamCity Kotlin DSL for an upcoming Medium article. 
+   
+Follow me on [Medium](https://clydedz.medium.com/) and [Twitter](https://twitter.com/clydedz).
---
+++ src/Home.tsx
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+
+function Home() {
+    return (
+        <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>
+            Home
+          </h1>
+          <p>Site update #1</p>
+        </header>
+      </div>
+    );
+}
+
+export default Home;
